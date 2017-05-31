61bb4188a751d397dc00eafecfb7091c6dea300b
Jasmine Chen <j.lin013@gmail.com>
2017-05-31 20:58:41 +0800

    Separate evaluation of ghosts and pacman. Implement primitive dynamic pacman tracking for ghosts

--- 30_0.0a.py
+++ 30_0.0a.py
@@ -77,6 +77,87 @@ class SmileAgent(CaptureAgent):
     def isGhost(self, agent):
         return agent > 4
 
+    def evaluationPacman(self, currentGameState, pacman = 0, action = Directions.STOP):
+        isRedSide = not self.red
+
+        nextGameState = currentGameState.generateSuccessor(pacman, action)
+        baseScores = [-300.0, -600.0, -100.0]
+        decayFacts = [0.3, 0.3, 0.3]
+
+        ghostIndices = [4, 6] if isRedSide else [5, 7]
+        currentPacmanPosition = currentGameState.getAgentPosition(pacman)
+        nextPacmanPosition = nextGameState.getAgentPosition(pacman)
+        currentGhostPositions = [currentGameState.getAgentPosition(g) for g in ghostIndices]
+        nextGhostPositions = [nextGameState.getAgentPosition(g) for g in ghostIndices]
+
+        score = 0.0
+
+        score += 10.0 * nextGameState.getScore()
+        if not isRedSide: score = -1.0 * score
+
+        foodList = nextGameState.getRedFood().asListNot() if isRedSide else nextGameState.getBlueFood().asListNot()
+        capsuleList = nextGameState.getCapsules()
+
+        score -= 200.0 * len(foodList)
+        score -= 500.0 * len(capsuleList)
+        for food in foodList:
+            dist = self.getMazeDistance(nextPacmanPosition, food)
+            score += baseScores[0] * (1.0 - math.exp(-1.0 * decayFacts[0] * dist))
+        for capsule in capsuleList:
+            dist = self.getMazeDistance(nextPacmanPosition, capsule)
+            score += baseScores[1] * (1.0 - math.exp(-1.0 * decayFacts[1] * dist))
+        for ghost in currentGhostPositions:
+            dist = self.getMazeDistance(nextPacmanPosition, ghost)
+            baseScore = baseScores[2] if dist >= 3 else -1e6
+            score += baseScore * math.exp(-1.0 * decayFacts[2] * dist)
+        if action == Directions.STOP: score -= 300.0
+
+        return score
+
+    def evaluationGhost(self, currentGameState, ghost, action = Directions.STOP):
+        isRedSide = self.red
+
+        nextGameState = currentGameState.generateSuccessor(ghost, action)
+        baseScores = [-300.0, -600.0, -100.0]
+        decayFacts = [0.3, 0.3, 0.3]
+
+        pacmanIndices = [1, 3] if isRedSide else [0, 2]
+        currentPacmanPositions = [currentGameState.getAgentPosition(p) for p in pacmanIndices]
+        nextPacmanPositions = [nextGameState.getAgentPosition(p) for p in pacmanIndices]
+        nextGhostPosition = nextGameState.getAgentPosition(ghost)
+
+        score = 0.0
+
+        score += 5.0 * nextGameState.getScore()
+        if not isRedSide: score = -1.0 * score
+
+        foodList = nextGameState.getRedFood().asListNot() if isRedSide else nextGameState.getBlueFood().asListNot()
+        capsuleList = nextGameState.getCapsules()
+
+        threats = []
+        for pacman in currentPacmanPositions: # calculate threat -> the lower, the worse for ghost
+            threats.append( sum(baseScores[0] * math.exp(-1.0 * decayFacts[0] * self.getMazeDistance(pacman, food)) for food in foodList) )
+            # threatCalibrated = threat * (1.0 - math.exp(-1.0 * decayFacts[2] * self.getMazeDistance(nextGhostPosition, pacman)))
+            # print('{} = {} => {} '.format(pacman, threat, threatCalibrated))
+            # score -= threatCalibrated
+
+        dist = 0
+        if threats[0] < threats[1]:
+            dist = self.getMazeDistance(nextGhostPosition, currentPacmanPositions[0])
+        else:
+            dist = self.getMazeDistance(nextGhostPosition, currentPacmanPositions[1])
+        score += baseScores[2] * math.exp(decayFacts[2] * dist)
+        """ Do not consider capsules temporarily
+        for capsule in capsuleList:
+            minDist = min(self.getMazeDistance(p, capsule) for p in nextPacmanPositions)
+            score += baseScores[1] * (math.exp(-1.0 * decayFacts[1] * minDist))
+        """
+
+        # print('ghost index = {}, score of {} is {}'.format(ghost, action, score))
+
+        return score
+
+
     # (isRedSide = True) the higher, the better for (blue) pacmans
     def evaluationFunction(self, currentGameState, isRedSide = True, agent = 0, action = Directions.STOP):
 
@@ -154,7 +235,7 @@ class SmileAgent(CaptureAgent):
         bestActions = []
         actions = gameState.getLegalActions(agent)
         for action in actions:
-            score = self.evaluationFunction(gameState, self.red ^ True, self.index[0], action)
+            score = self.evaluationPacman(gameState, self.index[0], action)
             if score > bestScore:
                 bestScore = score
                 bestActions = [action]
@@ -171,7 +252,7 @@ class SmileAgent(CaptureAgent):
         scoresCalibrated = []
         for action in actions:
             s = gameState.generateSuccessor(agent, action)
-            score = self.evaluationFunction(gameState, self.red ^ True, self.index[0], action)
+            score = self.evaluationPacman(gameState, self.index[0], action)
             depthSurvive = depth + 1
             for i in range(depth):
                 result = self.chooseActionGhost(s, ghostIndices[0], agent)
@@ -192,6 +273,7 @@ class SmileAgent(CaptureAgent):
 
 
     def chooseAction(self, gameState):
+        print('- Choose Action: ')
         bestScore = -1e100
         bestActions = []
         decisions = []
@@ -199,8 +281,7 @@ class SmileAgent(CaptureAgent):
         if self.role == 0: return self.chooseActionSafer(gameState, self.index[0], 2)
         actions = gameState.getLegalActions(self.index[self.role])
         for action in actions:
-            score = self.evaluationFunction(gameState, self.red ^ (self.role == 0), self.index[self.role], action)
-            if self.role == 1: score = -1.0 * score
+            score = self.evaluationGhost(gameState, self.index[self.role], action)
             if score > bestScore:
                 bestScore = score
                 bestActions = [action]
